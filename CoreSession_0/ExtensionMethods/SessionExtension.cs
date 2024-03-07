560d8897c267a748d718ce6fed2053282270f021
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-03-07 14:15:21 +0300

    Add project files.

---
+++ CoreSession_0/ExtensionMethods/SessionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace CoreSession_0.ExtensionMethods
+{
+    public static class SessionExtension
+    {
+        //Bir metodun Extension method olabilmesi icin ilk parametresini cok özel alması gerekir...Bu ilk parametre verilirken this keyword'u ile baslanmalıdır...Ve 
+
+
+        public static void SetObject(this ISession session, string key, object value)
+        {
+            string serializedObject = JsonConvert.SerializeObject(value);
+            session.SetString(key, serializedObject);
+        }
+
+        public static T GetObject<T>(this ISession session,string key) where T : class
+        {
+            string serializedObject = session.GetString(key);   
+            if(string.IsNullOrEmpty(serializedObject))
+            {
+                return null;
+            }
+            T deserializedObject = JsonConvert.DeserializeObject<T>(serializedObject);
+            return deserializedObject;
+        }
+    }
+}
